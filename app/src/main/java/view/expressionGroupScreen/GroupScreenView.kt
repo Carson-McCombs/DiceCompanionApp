87e0fb4e284a8e26a248ad9890fcc2b45745ea8d
Carson-McCombs <carson.mccombs.work@gmail.com>
2024-07-02 13:52:24 -0500

    Swapped Copy and Paste buttons to use vector icons ( also swapped delete button to outlined version )

--- app/src/main/java/view/expressionGroupScreen/GroupScreenView.kt
+++ app/src/main/java/view/expressionGroupScreen/GroupScreenView.kt
@@ -8,7 +8,6 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -25,9 +23,8 @@ import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.FloatingActionButton
@@ -45,11 +42,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import com.carsonmccombs.dicecompanion.R
 import model.dataObjects.ClipboardReference
 import model.dataObjects.Expression
 import model.dataObjects.Group
@@ -313,25 +311,28 @@ private fun SelectionOptions(
         horizontalArrangement = Arrangement.End,
         verticalAlignment = Alignment.CenterVertically
     ){
-        Button(
-            contentPadding = PaddingValues(0.dp),
+        IconButton(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(defaultIconButtonPadding),
             onClick = copy
         ) {
-            Text(
-                text = "Copy",
-                color = Color.Black,
-                fontSize = MaterialTheme.typography.labelSmall.fontSize
+            Icon(
+                modifier = Modifier.size(defaultIconSize),
+                painter = painterResource(id = R.drawable.baseline_content_copy_24),
+                contentDescription = "Copies to Clipboard"
             )
         }
-        Spacer(Modifier.width(defaultIconButtonPadding * 4))
-        Button(
-            contentPadding = PaddingValues(0.dp),
+        IconButton(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(defaultIconButtonPadding),
             onClick = paste
         ) {
-            Text(
-                text = "Paste",
-                color = Color.Black,
-                fontSize = MaterialTheme.typography.labelSmall.fontSize
+            Icon(
+                modifier = Modifier.size(defaultIconSize),
+                painter = painterResource(id = R.drawable.baseline_content_paste_24),
+                contentDescription = "Pastes from Clipboard"
             )
         }
         IconButton(
@@ -341,7 +342,8 @@ private fun SelectionOptions(
             onClick = delete
         ) {
             Icon(
-                imageVector = Icons.Default.Delete,
+                modifier = Modifier.size(defaultIconSize),
+                imageVector = Icons.Outlined.Delete,
                 contentDescription = "Deletes Selection"
             )
         }
